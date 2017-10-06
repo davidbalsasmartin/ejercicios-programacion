64bed0fbf2d0e0599b03d33e678353f9a73b303e
David Balsas Martín <dasdadbm@gmail.com>
2017-10-06 09:20:30 +0200

    Actulizando

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,9 @@
 /**
- * Autor = David Balsas Martín
+ * Autor = David Balsas
  */
 public class HolaMundo { // Clase principal
   public static void main(String[] args) {
-    System.out.println("Hola Mundo!");
+    System.out.println("\033[34mHola Mundo!");
+    System.out.println("\033[33mPrograma realizado por David Balsas Martín!");
   }
 }
