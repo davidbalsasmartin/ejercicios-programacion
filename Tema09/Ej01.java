59e155e90c3050ac7958c42d1b8b086fc9624f2d
davidbalsasmartin <dasdadbm@gmail.com>
2018-02-06 12:04:27 +0100

    poo

---
+++ Tema09/Ej01.java
@@ -0,0 +1,19 @@
+package tema09;
+/**
+ * Capítulo 9, Ejercicio 1
+ *
+ * @author David.B.M.
+ */
+
+public class Ej01 {
+
+  public static void main(String[] args) {
+    
+    
+    Caballo a = new Caballo("blanco", "pura", 600.50 , 195, 50);
+    
+    System.out.println(a);
+    a.relincha();
+  }
+
+}
